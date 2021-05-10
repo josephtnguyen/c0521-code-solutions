74ab9856b071ca244b6e54f3a0d0edd5eb656ae9
Joseph Nguyen <josephtringuyen2@gmail.com>
2021-05-10 11:36:06 -0700

    combined variable declarations with initialization

--- javascript-primitives-and-variables/main.js
+++ javascript-primitives-and-variables/main.js
@@ -1,10 +1,6 @@
-var fullName, isCool, totalPets;
-var unknown;
-var never;
-
-fullName = 'Joseph Nguyen';
-isCool = true;
-totalPets = 0;
+var fullName = 'Joseph Nguyen';
+var isCool = true;
+var totalPets = 0;
 
 console.log('value of fullName:', fullName);
 console.log('value of isCool:', isCool);
@@ -14,10 +10,12 @@ console.log('typeof fullName:', typeof (fullName));
 console.log('typeof isCool:', typeof (isCool));
 console.log('typeof totalPets:', typeof (totalPets));
 
-unknown = null;
+var unknown = null;
 
 console.log('value of unknown:', unknown);
 console.log('typeof unknown:', typeof (unknown));
+
+var never;
 
 console.log('value of never:', never);
 console.log('typeof never', typeof never);
