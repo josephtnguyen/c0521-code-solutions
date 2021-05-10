f00fb02e6e8b76e1d0fd3ab21ac533c3c7035692
Joseph Nguyen <josephtringuyen2@gmail.com>
2021-05-10 10:41:15 -0700

    completed javascript-primitives-and-variables

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,23 @@
+var fullName, isCool, totalPets;
+var unknown;
+var never;
+
+fullName = 'Joseph Nguyen';
+isCool = true;
+totalPets = 0;
+
+console.log('value of fullName:', fullName);
+console.log('value of isCool:', isCool);
+console.log('value of totalPets:', totalPets);
+
+console.log('typeof fullName:', typeof (fullName));
+console.log('typeof isCool:', typeof (isCool));
+console.log('typeof totalPets:', typeof (totalPets));
+
+unknown = null;
+
+console.log('value of unknown:', unknown);
+console.log('typeof unknown:', typeof (unknown));
+
+console.log('value of never:', never);
+console.log('typeof never', typeof never);
