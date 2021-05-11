12c47466903c276cad276d601f3157e739a4a1ee
Joseph Nguyen <josephtringuyen2@gmail.com>
2021-05-11 10:56:31 -0700

    completed javascript-data-model

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,86 @@
+var orderHistory = [
+  {
+    orderNum: [114, 3941689, 8772232],
+    orderDate: [4, 8, 2020],
+    deliverDate: [8, 8, 2020],
+    returnWindow: [7, 9, 2020],
+    shipTo: 'JS Masher',
+    handed: false,
+    total: 34.00,
+    items: [
+      {
+        title: 'JavaScript for impatient programmers',
+        price: 31.55,
+        author: {
+          firstName: 'Axel',
+          lastName: 'Rauschmayer',
+          prefix: 'Dr.',
+          suffix: null
+        }
+      }
+    ]
+  },
+  {
+    orderNum: [113, 9984268, 1280257],
+    orderDate: [19, 7, 2020],
+    deliverDate: [20, 7, 2020],
+    returnWindow: [19, 8, 2020],
+    shipTo: 'JS Masher',
+    handed: true,
+    total: 44.53,
+    items: [
+      {
+        title: 'The Timeless Way of Building',
+        price: 41.33,
+        author: {
+          firstName: 'Christopher',
+          lastName: 'Alexander',
+          prefix: null,
+          suffix: null
+        }
+      }
+    ]
+  },
+  {
+    orderNum: [114, 2875557, 9059409],
+    orderDate: [4, 7, 2020],
+    deliverDate: [7, 7, 2020],
+    returnWindow: [5, 8, 2020],
+    shipTo: 'JS Masher',
+    handed: true,
+    total: 17.22,
+    items: [
+      {
+        title: 'Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for WII U, PC. Support Turbo and Vibration Features. No Driver and No Lag-Gamecube Adapter',
+        price: 15.98,
+        author: null
+      }
+    ]
+  },
+  {
+    orderNum: [113, 2883177, 2648248],
+    orderDate: [3, 7, 2020],
+    deliverDate: [5, 7, 2020],
+    returnWindow: [4, 8, 2020],
+    shipTo: 'JS Masher',
+    handed: false,
+    total: 138.93,
+    items: [
+      {
+        title: 'GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)',
+        price: 94.95,
+        author: null
+      },
+      {
+        title: 'The Art of Sql',
+        price: 33.99,
+        author: {
+          firstName: 'Stephanie',
+          lastName: 'Faroult',
+          prefix: null,
+          suffix: null
+        }
+      }
+    ]
+  }
+];
