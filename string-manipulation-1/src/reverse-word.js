13bd00170fdf38b1b02e274bea52cdf4a98d6825
Joseph Nguyen <josephtringuyen2@gmail.com>
2021-05-13 15:27:17 -0700

    condensed code in reverse-word.js

--- string-manipulation-1/src/reverse-word.js
+++ string-manipulation-1/src/reverse-word.js
@@ -1,8 +1,8 @@
 /* exported reverseWord */
 function reverseWord(word) {
   var newWord = '';
-  for (var i = 0; i < word.length; i++) {
-    newWord += word[word.length - 1 - i];
+  for (var i = word.length - 1; i >= 0; i--) {
+    newWord += word[i];
   }
   return newWord;
 }
