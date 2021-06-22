86afde300bb276419595691fce31d2209c976ded
Joseph Nguyen <josephtringuyen2@gmail.com>
2021-06-22 16:54:28 -0700

    completed node-module-system

---
+++ node-module-system/a-wrapped-module.js
@@ -0,0 +1,14 @@
+console.log(`exports type: ${typeof exports}`);
+console.log(`exports value: ${exports}`);
+
+console.log(`require type: ${typeof require}`);
+console.log(`require value: ${require}`);
+
+console.log(`module type: ${typeof module}`);
+console.log(`module value: ${module}`);
+
+console.log(`__filename type: ${typeof __filename}`);
+console.log(`__filename value: ${__filename}`);
+
+console.log(`__dirname type: ${typeof __dirname}`);
+console.log(`__dirname value: ${__dirname}`);
