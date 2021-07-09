36077cae474c3eec85468e6a2591b993cdb820b9
Joseph Nguyen <josephtringuyen2@gmail.com>
2021-07-08 17:47:33 -0700

    completed react-events-and-state

---
+++ react-events-and-state/src/index.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class CustomButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { isClicked: false };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(e) {
+    this.setState({ isClicked: true });
+  }
+
+  render() {
+    if (!this.state.isClicked) {
+      return (
+        <button onClick={this.handleClick}>Click Me!</button>
+      );
+    } else {
+      return (
+        <button>Thanks!</button>
+      );
+    }
+  }
+}
+
+ReactDOM.render(<CustomButton />, document.querySelector('#root'));
