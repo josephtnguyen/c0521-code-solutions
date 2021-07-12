5009337a8fdefe8214a204088c30a6f6f2e194f2
Joseph Nguyen <josephtringuyen2@gmail.com>
2021-07-12 09:55:15 -0700

    made new classes for coloring icons

--- react-validated-input-component/src/icon.jsx
+++ react-validated-input-component/src/icon.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 export default class Icon extends React.Component {
   render() {
     const validPassword = this.props.validPassword;
-    const check = <i className="fa fa-check" />;
-    const x = <i className="fa fa-times" />;
+    const check = <i className="fa fa-check green" />;
+    const x = <i className="fa fa-times red" />;
     const icon = validPassword ? check : x;
     return (
       <span className="icon">
